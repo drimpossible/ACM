207a835676b0639a87dbac13c021ab60a47e72ba
drimpossible <drimpossible@mailfence.com>
2023-08-05 23:19:50 +0100

    started fixing code for release

--- src/run_blind.py
+++ src/run_blind.py
@@ -2,6 +2,7 @@ import multiprocessing
 import numpy as np
 from scipy import stats
 from os.path import exists
+import h5py, os
 
 def load_filelist(filepath):
     imglist, y = [], []
@@ -11,39 +12,52 @@ def load_filelist(filepath):
             y.append(int(label)) 
     return imglist, y
 
-def get_preds(shift, k):
+
+def get_preds(k):
     # TODO: Further optimize the stats.mode calculation by a running mode, this code is very slow for very large k which can hinder analysis
     for i in range(1, len(labels)):
-        if i <= k+shift:
+        if i <= k:
             pred[i-1] = labels[0]  
         else:
-            pred[i-1] = stats.mode(labels[i-(k+shift):(i-shift)])[0][0]
-    np.save(LOGDIR+'/shift_'+prefix+'_blind_preds_'+str(k)+'_'+str(shift)+'.npy', pred)
+            pred[i-1] = stats.mode(labels[i-(k):(i)])[0]
+    np.save(logdir+'/shift_'+dataset+'_blind_preds_'+str(k)+'.npy', pred)
     return pred
 
 if __name__ == '__main__':
-    prefix='cglm'
-    #prefix='cloc'
+    dataset='CGLM'
+    #dataset='CLOC'
 
-    ORDERFILEDIR='../data/'+prefix+'/' 
-    LOGDIR='../data/blind/'
+    num_processes = 16
 
-    _, labels = load_filelist(filepath=ORDERFILEDIR+'/train.txt')
-    labels = np.array(labels, dtype='u2')
+    # Please set directory for labels in the CLDatasets folder
+    path_to_cldatasets = '/media/bayesiankitten/Hanson/CLDatasets/data/'
+    
+    # Please set directory for logs
+    global logdir, labels 
+    logdir = '/media/bayesiankitten/Alexander/ACM/blind_logs/'
+    os.makedirs(logdir, exist_ok=True)
+    
+    # Load labels file
+    # Note: When actually picking the k, use pretrain_labels.hdf5 file. This is for analysis plots, shown in the paper (both have same results, but different purpose).
+    with h5py.File(f'{path_to_cldatasets}/{dataset}/order_files/train_labels.hdf5', 'r') as f:
+        labels = np.array(f['store_list'])
+
+    labels = np.array(labels, dtype=np.uint16)
     pred = np.ones_like(labels)[1:]
 
     # Get dataset mode
-    modelabel = stats.mode(labels)[0][0]
+    modelabel = stats.mode(labels)[0]
     pred = pred*modelabel
-    np.save(LOGDIR+'/'+prefix+'_mode.npy', pred)
+    np.save(logdir+'/'+dataset+'_mode.npy', pred)
 
-    p = multiprocessing.Pool(16)
+    p = multiprocessing.Pool(num_processes)
     result = []
-    for k in [1, 2, 5, 10, 20, 30, 40, 60, 75, 100, 250, 500, 1000, 5000, 25000, 75000]:
-            print(k, shift)
+    for k in [1, 2, 3, 5, 7, 10, 20, 25, 35, 50, 75, 100, 150, 250, 500, 750, 1000, 2500, 5000, 7500, 10000, 15000, 25000, 50000, 75000]:
+            print(f'Processing mode of past: {k}')
             pred = np.ones_like(labels)[1:]
-            if not exists(LOGDIR+'/shift_'+prefix+'_blind_preds_'+str(k)+'_'+str(shift)+'.npy'): 
-                result.append(p.apply_async(get_preds, [shift,k]))
+
+            if not exists(logdir+'/shift_'+dataset+'_blind_preds_'+str(k)+'.npy'): 
+                result.append(p.apply_async(get_preds, [k]))
 
     for r in result:
         r.wait()

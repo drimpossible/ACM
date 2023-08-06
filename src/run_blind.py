ef71a5afb54844929ecf9620b3fb196739685921
drimpossible <drimpossible@mailfence.com>
2023-08-06 16:28:48 +0100

    Final blind classifier code

--- src/run_blind.py
+++ src/run_blind.py
@@ -14,27 +14,23 @@ def load_filelist(filepath):
 
 
 def get_preds(k):
-    # TODO: Further optimize the stats.mode calculation by a running mode, this code is very slow for very large k which can hinder analysis
+    pred = np.ones_like(labels)[1:]
+
     for i in range(1, len(labels)):
         if i <= k:
             pred[i-1] = labels[0]  
         else:
             pred[i-1] = stats.mode(labels[i-(k):(i)])[0]
-    np.save(logdir+'/shift_'+dataset+'_blind_preds_'+str(k)+'.npy', pred)
+    np.save(logdir+'/'+dataset+'_blind_preds_'+str(k)+'.npy', pred)
     return pred
 
+
 if __name__ == '__main__':
-    dataset='CGLM'
-    #dataset='CLOC'
-
-    num_processes = 16
-
-    # Please set directory for labels in the CLDatasets folder
-    path_to_cldatasets = '/media/bayesiankitten/Hanson/CLDatasets/data/'
-    
-    # Please set directory for logs
     global logdir, labels 
-    logdir = '/media/bayesiankitten/Alexander/ACM/blind_logs/'
+    dataset='CLEAR100' # Choose one from: ['CLEAR10', 'CLEAR100', 'CGLM', 'CLOC']
+    num_processes = 16
+    path_to_cldatasets = '/media/bayesiankitten/Hanson/CLDatasets/data/' # Please set directory for labels in the CLDatasets folder
+    logdir = '/media/bayesiankitten/Alexander/ACM/blind_logs/' # Please set directory for logs
     os.makedirs(logdir, exist_ok=True)
     
     # Load labels file
@@ -45,21 +41,36 @@ if __name__ == '__main__':
     labels = np.array(labels, dtype=np.uint16)
     pred = np.ones_like(labels)[1:]
 
-    # Get dataset mode
-    modelabel = stats.mode(labels)[0]
-    pred = pred*modelabel
-    np.save(logdir+'/'+dataset+'_mode.npy', pred)
-
-    p = multiprocessing.Pool(num_processes)
-    result = []
+    # Get dataset mode, get performance to check dataset imbalance
+    if not exists(logdir+'/'+dataset+'_mode.npy'):
+        modelabel = stats.mode(labels)[0]
+        pred = pred*modelabel
+        np.save(logdir+'/'+dataset+'_mode.npy', pred)
+    
+    pred = np.load(logdir+'/'+dataset+'_mode.npy')
+    gt = labels[1:]
+    acc = np.equal(gt,pred)*1.0
+    idx = np.arange(acc.shape[0])+1
+    cumacc = np.cumsum(acc)/idx
+    print(f'IID Best Classifier: {cumacc.mean()}')
+    
+    # Get blind classifier performance
     for k in [1, 2, 3, 5, 7, 10, 20, 25, 35, 50, 75, 100, 150, 250, 500, 750, 1000, 2500, 5000, 7500, 10000, 15000, 25000, 50000, 75000]:
+        if not exists(logdir+'/'+dataset+'_blind_preds_'+str(k)+'.npy'):
             print(f'Processing mode of past: {k}')
-            pred = np.ones_like(labels)[1:]
-
-            if not exists(logdir+'/shift_'+dataset+'_blind_preds_'+str(k)+'.npy'): 
-                result.append(p.apply_async(get_preds, [k]))
-
-    for r in result:
-        r.wait()
+            p = multiprocessing.Pool(num_processes)
+            result = [] 
+            result.append(p.apply_async(get_preds, [k]))
+            for r in result:
+                r.wait()
+            
+    # Show results, see the degree of label correlation        
+    for k in [1, 2, 3, 5, 7, 10, 20, 25, 35, 50, 75, 100, 150, 250, 500, 750, 1000, 2500, 5000, 7500, 10000, 15000, 25000, 50000, 75000]:        
+        pred = np.load(logdir+'/'+dataset+'_blind_preds_'+str(k)+'.npy')
+        gt = labels[1:]
+        acc = np.equal(gt,pred)*1.0
+        idx = np.arange(acc.shape[0])+1
+        cumacc = np.cumsum(acc)/idx
+        print(f'Blind Classifier @ {k}: {cumacc.mean()}')
 
     print('Extracted all blind classifier results!')
